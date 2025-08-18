49c8ebcd6934f785bf2c6e02e659522f76b9af11
Mariana Guerra <marianahguerra@gmail.com>
2025-08-17 22:14:47 -0300

    Criar diretório e arquivo para nmain.tf para nova resource instancia

---
+++ testeInstancia/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = ami-0169aa51f6faf20d5
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "servidorpepino"
+  }
+}
