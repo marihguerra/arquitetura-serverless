970fe733debb6bbbf5e14bc041ed341e7fd4d5ae
Mariana Guerra <marianahguerra@gmail.com>
2025-08-26 19:26:27 -0300

    CorreÃigir valor do ami e subir intancia ec2

--- teste_ec2.tf
+++ teste_ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = ami-0169aa51f6faf20d5
+  ami           = "ami-0169aa51f6faf20d5"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "servidorpepino"
+    Name = "My instance"
   }
 }
