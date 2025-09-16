c9798df44ef7b3c247d735d54cf6a2af830dc0b3
Mariana Guerra <marianahguerra@gmail.com>
2025-09-16 15:49:02 -0300

    Criar tabela DynamoDB e o mapeamento da fila sqs para a process lambda

--- main.tf
+++ main.tf
@@ -15,23 +15,25 @@ resource "aws_sqs_queue" "my_queue" {
   name = "orders_queue"
 }
 
-# resource "aws_dynamodb_table" "my_table" {
-#   name           = "orders_table"
-#   billing_mode   = "PAY_PER_REQUEST"
-#   hash_key       = "id"
-#
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
-# }
-#
-# resource "aws_lambda_event_source_mapping" "sqs_to_lambda2" {
-#   event_source_arn = aws_sqs_queue.orders_queue.arn
-#   function_name    = aws_lambda_function.lambda2.arn
-#   batch_size       = 10
-# }
-#
+resource "aws_dynamodb_table" "my_table" {
+  name           = "orders_table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "orderId"
+
+  attribute {
+    name = "orderId"
+    type = "S"
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_to_lambda2" {
+  event_source_arn                   = "arn:aws:sqs:us-east-2:444219106888:orders_queue"
+  function_name                      = "process_lambda"
+  batch_size                         = 10
+  enabled                            = true
+  maximum_batching_window_in_seconds = 5
+}
+
 # resource "aws_cloudwatch_metric_alarm" "dynamodb_insert_alarm" {
 #   alarm_name          = "DynamoDBInsertAlarm"
 #   comparison_operator = "GreaterThanThreshold"
